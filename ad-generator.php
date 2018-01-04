835bbca4d946c7033b70324a71835dec53536352
Airat Halitov <airat.halitov@gmail.com>
2018-01-04 16:09:26 +0500

    Добавил библиотеку для генерации

--- ad-generator.php
+++ ad-generator.php
@@ -12,3 +12,54 @@
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+// https://habrahabr.ru/company/dataart/blog/265245/
+// [ad_generator]
+
+class ad_generator_shortcode {
+	
+	static $add_script;
+	
+	static function init () {
+		add_shortcode('ad_generator', array(__CLASS__, 'ad_generator_func'));
+		//add_action('init', array(__CLASS__, 'register_script'));
+		add_action('wp_footer', array(__CLASS__, 'print_script'));
+	}
+	
+	static function ad_generator_func( $atts ) {
+		self::$add_script = true; 
+		return "foo and bar";
+	}
+	
+	//static function register_script() {	}
+	
+	static function print_script () {
+		if ( !self::$add_script ) return;
+		
+		echo '<form method="post" action="">';
+		$ad_text = isset($_POST['ad_text']) ? (string) $_POST['ad_text'] : '';
+		
+		if ($ad_text) {
+			echo '<textarea name="ad_text" cols="100" rows="14">' . htmlspecialchars($ad_text) . '</textarea>';
+		} else {
+			echo '<textarea name="ad_text" cols="100" rows="14">{Рандомизатор|Рандомайзер} {|текста}</textarea>';
+		} 
+		
+		echo '<br /><input type="submit" value="Генерировать" /></form>'
+		
+		if ($ad_text) {
+			require_once plugin_dir_path( __FILE__ ).'/includes/Natty/TextRandomizer.php';
+			
+			$tRand = new Natty_TextRandomizer($ad_text);
+			echo '<p>Число всех возможных вариантов: <strong>' . $tRand->numVariant(). '</strong>. Из них случайные 10:</p>';
+			
+			for ($i=0; $i<10; ++$i) {
+				echo htmlspecialchars($tRand->getText()) . '<hr />';
+			}
+		}
+	}
+}
+
+
+ad_generator_shortcode::init();
+
